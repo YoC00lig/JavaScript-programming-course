b49350daa46a6e04063505e6be89d0a33149efc4
YoC00lig <kulig.j@vp.pl>
2023-04-25 23:23:05 +0200

    :c

---
+++ lab05/zad2/src/app2.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
+/* *************************** */
+/* Configuring the application */
+/* *************************** */
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(__dirname + '/../public'));
+app.use(express.urlencoded({ extended: false }));
+
+
+app.set('views', __dirname + '/../views'); // Files with views can be found in the 'views' directory
+app.set('view engine', 'pug'); // Use the 'Pug' template system
+app.locals.pretty = app.get('env') === 'development'; // The resulting HTML code will be indented in the development environment
+/* ************************************************ */
+app.use(morgan('dev'));
+/* ******** */
+/* "Routes" */
+/* ******** */
+app.get('/', function (request, response) {
+    response.render('index'); // Render the 'index' view
+});
+
+app.post('/', function (request, response) {
+    const name = request.body.name || 'world';
+    response.setHeader('Content-Type', 'text/plain');
+    response.send(`Hello ${name}`);
+});
+
+/* ************************************************ */
+app.listen(8000, function () {
+    console.log('The server was started on port 8000');
+    console.log('To stop the server, press "CTRL + C"');
+});          
